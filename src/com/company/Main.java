81d2a6812e116c38cae9d46c4a943fc2fefda42a
FaeRof <chernysheya@mail.ru>
2021-03-11 10:33:12 +0300

    Prettier code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,83 +11,81 @@ public class Main {
         String url = "jdbc:mysql://localhost:3306/my_schema";
         Class.forName("com.mysql.cj.jdbc.Driver");
         Scanner scanner = new Scanner(System.in);
-        ResultSet resultSet=null;
+        ResultSet resultSet = null;
         try (Connection connection = DriverManager.getConnection(url, username, password);
-          Statement statement = connection.createStatement()) {
+             Statement statement = connection.createStatement()) {
 
             System.out.println("\nPlease select operation by number: \n 1.Create a book database on this computer " +
                     "\n 2.Delete old and create new book database \n 3.Work with what is in the book database ");
             int x = Integer.parseInt(scanner.nextLine());
-            if (x<4){
-            switch (x) {
+            if (x < 4) {
+                switch (x) {
 
 
-                case (1):
-                    statement.execute("CREATE TABLE if not exists book (id MEDIUMINT not null  auto_increment," +
-                            " name char(100) not null,page int DEFAULT 0,publication DATE,author char(100) not null, PRIMARY KEY (id));");
-                    statement.execute("insert into book (name,page, publication,author) " +
-                            "values" +
-                            "('Inferno', 544, {d '2013-05-14'},'Dan Brown')," +
-                            "('Solomon Kane',432,{d '1929-01-10'},'Robert E. Howard')," +
-                            "('Lord of the Rings',1186,{d '1954-07-29'},'J.R.R.Tolkien')," +
-                            "('Harry Potter and the Sorcerer`s Stone',336,{d '1997-06-26'},'Joanne Rowling')," +
-                            "('And Then There Were None',256,{d '1939-10-16'},'Agatha Christie')," +
-                            "('Twenty Thousand Leagues Under the Sea',244,{d '1870-06-20'},'Jules Verne')," +
-                            "('Don Quixote',863,{d '1605-01-01'},'Miguel de Cervantes')," +
-                            "('The Divine Comedy',798,{d '1472-04-11'},'Dante Alighieri')");
-                    resultSet = statement.executeQuery("select * from book");
-                    while (resultSet.next()) {
-                        int id = resultSet.getInt(1);
-                        String name = resultSet.getString(2);
-                        int page = resultSet.getInt(3);
-                        String publication = resultSet.getString(4);
-                        String author = resultSet.getString(5);
-                        System.out.printf("%d. %s - %d.pages / %s - %s \n", id, name, page, publication, author);
-                    }
-                    break;
-                case (2):
-                    statement.execute("drop table if exists book");
-                    statement.execute("CREATE TABLE if not exists book (id MEDIUMINT not null  auto_increment," +
-                            " name char(100) not null,page int DEFAULT 0,publication DATE,author char(100) not null, PRIMARY KEY (id));");
-                    statement.execute("insert into book (name,page, publication,author) " +
-                            "values" +
-                            "('Inferno', 544, {d '2013-05-14'},'Dan Brown')," +
-                            "('Solomon Kane',432,{d '1929-01-10'},'Robert E. Howard')," +
-                            "('Lord of the Rings',1186,{d '1954-07-29'},'J.R.R.Tolkien')," +
-                            "('Harry Potter and the Sorcerer`s Stone',336,{d '1997-06-26'},'Joanne Rowling')," +
-                            "('And Then There Were None',256,{d '1939-10-16'},'Agatha Christie')," +
-                            "('Twenty Thousand Leagues Under the Sea',244,{d '1870-06-20'},'Jules Verne')," +
-                            "('Don Quixote',863,{d '1605-01-01'},'Miguel de Cervantes')," +
-                            "('The Divine Comedy',798,{d '1472-04-11'},'Dante Alighieri')");
-                    resultSet = statement.executeQuery("select * from book");
-                    while (resultSet.next()) {
-                        int id = resultSet.getInt(1);
-                        String name = resultSet.getString(2);
-                        int page = resultSet.getInt(3);
-                        String publication = resultSet.getString(4);
-                        String author = resultSet.getString(5);
-                        System.out.printf("%d. %s - %d.pages / %s - %s \n", id, name, page, publication, author);
-                    }
-                    break;
-                case (3):
-                    resultSet = statement.executeQuery("select * from book");
-                    while (resultSet.next()) {
-                        int id = resultSet.getInt(1);
-                        String name = resultSet.getString(2);
-                        int page = resultSet.getInt(3);
-                        String publication = resultSet.getString(4);
-                        String author = resultSet.getString(5);
-                        System.out.printf("%d. %s - %d.pages / %s - %s \n", id, name, page, publication, author);
-                    }
-                    break;
+                    case (1):
+                        statement.execute("CREATE TABLE if not exists book (id MEDIUMINT not null  auto_increment," +
+                                " name char(100) not null,page int DEFAULT 0,publication DATE,author char(100) not null, PRIMARY KEY (id));");
+                        statement.execute("insert into book (name,page, publication,author) " +
+                                "values" +
+                                "('Inferno', 544, {d '2013-05-14'},'Dan Brown')," +
+                                "('Solomon Kane',432,{d '1929-01-10'},'Robert E. Howard')," +
+                                "('Lord of the Rings',1186,{d '1954-07-29'},'J.R.R.Tolkien')," +
+                                "('Harry Potter and the Sorcerer`s Stone',336,{d '1997-06-26'},'Joanne Rowling')," +
+                                "('And Then There Were None',256,{d '1939-10-16'},'Agatha Christie')," +
+                                "('Twenty Thousand Leagues Under the Sea',244,{d '1870-06-20'},'Jules Verne')," +
+                                "('Don Quixote',863,{d '1605-01-01'},'Miguel de Cervantes')," +
+                                "('The Divine Comedy',798,{d '1472-04-11'},'Dante Alighieri')");
+                        resultSet = statement.executeQuery("select * from book");
+                        while (resultSet.next()) {
+                            int id = resultSet.getInt(1);
+                            String name = resultSet.getString(2);
+                            int page = resultSet.getInt(3);
+                            String publication = resultSet.getString(4);
+                            String author = resultSet.getString(5);
+                            System.out.printf("%d. %s - %d.pages / %s - %s \n", id, name, page, publication, author);
+                        }
+                        break;
+                    case (2):
+                        statement.execute("drop table if exists book");
+                        statement.execute("CREATE TABLE if not exists book (id MEDIUMINT not null  auto_increment," +
+                                " name char(100) not null,page int DEFAULT 0,publication DATE,author char(100) not null, PRIMARY KEY (id));");
+                        statement.execute("insert into book (name,page, publication,author) " +
+                                "values" +
+                                "('Inferno', 544, {d '2013-05-14'},'Dan Brown')," +
+                                "('Solomon Kane',432,{d '1929-01-10'},'Robert E. Howard')," +
+                                "('Lord of the Rings',1186,{d '1954-07-29'},'J.R.R.Tolkien')," +
+                                "('Harry Potter and the Sorcerer`s Stone',336,{d '1997-06-26'},'Joanne Rowling')," +
+                                "('And Then There Were None',256,{d '1939-10-16'},'Agatha Christie')," +
+                                "('Twenty Thousand Leagues Under the Sea',244,{d '1870-06-20'},'Jules Verne')," +
+                                "('Don Quixote',863,{d '1605-01-01'},'Miguel de Cervantes')," +
+                                "('The Divine Comedy',798,{d '1472-04-11'},'Dante Alighieri')");
+                        resultSet = statement.executeQuery("select * from book");
+                        while (resultSet.next()) {
+                            int id = resultSet.getInt(1);
+                            String name = resultSet.getString(2);
+                            int page = resultSet.getInt(3);
+                            String publication = resultSet.getString(4);
+                            String author = resultSet.getString(5);
+                            System.out.printf("%d. %s - %d.pages / %s - %s \n", id, name, page, publication, author);
+                        }
+                        break;
+                    case (3):
+                        resultSet = statement.executeQuery("select * from book");
+                        while (resultSet.next()) {
+                            int id = resultSet.getInt(1);
+                            String name = resultSet.getString(2);
+                            int page = resultSet.getInt(3);
+                            String publication = resultSet.getString(4);
+                            String author = resultSet.getString(5);
+                            System.out.printf("%d. %s - %d.pages / %s - %s \n", id, name, page, publication, author);
+                        }
+                        break;
 
-            }
-            }else if (x>=4){
+                }
+            } else if (x >= 4) {
                 System.out.println("Unknown operation. Please, try again.");
                 return;
             }
-
-
 
 
 //
@@ -286,57 +284,59 @@ public class Main {
                         default:
                             System.out.println("Unknown index. Please, try again.");
                     }
+                    break;
+
+
+                case (4):
+                    /**
+                     ************************ Delete *********************************
+                     */
+                    System.out.println("\nDeleting modes(select by number): " +
+                            "\n 1.Delete by name: " +
+                            "\n 2.Delete book by range of page  ");
+
+                    int deleteIndex = Integer.parseInt(scanner.nextLine());
+                    switch (deleteIndex) {
+                        case (1):
+                            System.out.println("You have selected the mode: 1.Delete by name: " +
+                                    "\nPlease indicate the book name ");
+                            String deleteName = scanner.nextLine();
+
+                            preparedStatement = connection.prepareStatement("DELETE FROM book " +
+                                    "WHERE name = ?");
+                            System.out.println("\n Trying to delete... ");
+                            preparedStatement.setString(1, deleteName);
+                            preparedStatement.executeUpdate();
+                            System.out.println("\n book was deleted successfully! ");
                             break;
+                        case (2):
+                            System.out.println("You have selected the mode: 2.Delete books by range of page " +
+                                    "\nPlease indicate the low limit of the page ");
+                            String deleteLowRangePage = scanner.nextLine();
+                            System.out.println("Please indicate the top limit of the page ");
+                            String deleteTopRangePage = scanner.nextLine();
 
+                            preparedStatement = connection.prepareStatement("DELETE FROM book " +
+                                    "WHERE page BETWEEN ? AND ?");
+                            System.out.println("\n Trying to delete... ");
+                            preparedStatement.setString(1, deleteLowRangePage);
+                            preparedStatement.setString(2, deleteTopRangePage);
+                            preparedStatement.executeUpdate();
+                            System.out.println("\n books were deleted successfully! ");
+                            break;
+                        default:
+                            System.out.println("Unknown index. Please, try again");
+                    }
+                    break;
+                default:
+                    System.out.println("Unknown operation. Please, try again.");
 
-                    case(4):
-                        /**
-                         ************************ Delete *********************************
-                         */
-                        System.out.println("\nDeleting modes(select by number): " +
-                                "\n 1.Delete by name: " +
-                                "\n 2.Delete book by range of page  ");
+                    connection.close();
+                    statement.close();
+            }
 
-                        int deleteIndex =  Integer.parseInt(scanner.nextLine());
-                        switch (deleteIndex){
-                            case (1):
-                                System.out.println("You have selected the mode: 1.Delete by name: " +
-                                        "\nPlease indicate the book name ");
-                                String deleteName = scanner.nextLine();
-
-                                preparedStatement = connection.prepareStatement("DELETE FROM book " +
-                                        "WHERE name = ?");
-                                System.out.println("\n Trying to delete... ");
-                                preparedStatement.setString(1, deleteName);
-                                preparedStatement.executeUpdate();
-                                System.out.println("\n book was deleted successfully! ");
-                                break;
-                            case (2):
-                                System.out.println("You have selected the mode: 2.Delete books by range of page " +
-                                        "\nPlease indicate the low limit of the page ");
-                                String deleteLowRangePage = scanner.nextLine();
-                                System.out.println("Please indicate the top limit of the page ");
-                                String deleteTopRangePage = scanner.nextLine();
-
-                                preparedStatement = connection.prepareStatement("DELETE FROM book " +
-                                        "WHERE page BETWEEN ? AND ?");
-                                System.out.println("\n Trying to delete... ");
-                                preparedStatement.setString(1, deleteLowRangePage);
-                                preparedStatement.setString(2, deleteTopRangePage);
-                                preparedStatement.executeUpdate();
-                                System.out.println("\n books were deleted successfully! ");
-                                break;
-                            default: System.out.println("Unknown index. Please, try again");
-                        }
-                        break;
-                    default:
-                        System.out.println("Unknown operation. Please, try again.");
-
-                        connection.close();
-                        statement.close();
-                }
-
-        }scanner.close();
+        }
+        scanner.close();
         resultSet.close();
     }
 
